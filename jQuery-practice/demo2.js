f30bd1f46a277c5b33fe8f33ad15cb25aed28e9d
ThriteenLi <a1363252276@126.com>
2017-07-17 16:25:30 +0800

    2017.7.17jQuery练习

--- jQuery-practice/demo2.js
+++ jQuery-practice/demo2.js
@@ -1,12 +1,13 @@
-    $(function(){
-		$(":input").focus(function(){
-			  //$(this).addClass("focus").val(this.value==this.defaultValue?"":this.value);
-			  $(this).addClass("focus");
-			  if(/*this.value*/$(this).val() == this.defaultValue){
-				  //$(this).val("");
-				  this.value = "";
-			  }
-		}).blur(function(){
-			 $(this).removeClass("focus").val(this.value==""?this.defaultValue:this.value);
+	$(function(){
+	    var $comment = $('#comment');//获取评论框
+	    $('.up').click(function(){ //向上按钮绑定单击事件
+		   if(!$comment.is(":animated")){//判断是否处于动画
+				$comment.animate({ scrollTop  : "-=50" } , 400);
+			}
+		})
+		$('.down').click(function(){//向下按钮绑定单击事件
+		   if(!$comment.is(":animated")){
+				$comment.animate({ scrollTop  : "+=50" } , 400);
+			}
 		});
-    });
+	});
